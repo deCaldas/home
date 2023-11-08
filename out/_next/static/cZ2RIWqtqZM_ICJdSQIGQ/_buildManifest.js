593f02f0848b33d861ab232c0e16d64dc156faa1
Diego Toro <d.caldascaridad@gmail.com>
2023-11-08 17:18:55 -0500

    Creating Model Window

---
+++ out/_next/static/cZ2RIWqtqZM_ICJdSQIGQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-7982f244f0d17fb6.js"],"/404":["static/chunks/pages/404-bf424c0c56ca6abb.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
