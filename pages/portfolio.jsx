74e35e883552c08c73402c9eb9aae7676c388b7c
Diego Toro Cárdenas <67610317+deCaldas@users.noreply.github.com>
2024-11-01 08:02:34 -0500

    [UPDATE] Added a 'Hello World' to the portfolio, web developer's blog, and other projects.

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -16,66 +16,72 @@ import blogs from '../public/images/jobs/deCaldas-blogsProject.png'
 /* Imgs Jobs */
 import webPage from '../public/images/jobs/network-contact.png'
 import webPageForSale from '../public/images/jobs/webPageForSale.png'
+import webDeveloperBlog from '../public/images/jobs/web-developer-blog.png'
 
 const Development = () => (
   <Layout title="portfolio">
     <Container mt="73" mb="23">
-      <Heading as="h3" fontSize={20} mb={4}>
-        {/*         <Box borderRadius="lg"
+      <Heading mt="7" as="h3" fontSize={20} mb={4}>
+        <Box borderRadius="lg"
           borderWidth={2}
           borderStyle="solid"
           mb={6}
           p={3}
           textAlign="center"
           bg={useColorModeValue('whiteAlpha.500', 'blackAlpha.500')}>
- */}
-        Jobs
-        {/*         </Box> */}
+            {' Hola Mundo!! '}
+        </Box> 
       </Heading>
+
+      <Divider my={6} />
+
+      <Heading as="h3" variant="section-title">
+        Work
+      </Heading>
+
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section delay={0.1}>
-          <WorkGridItem
-            id="forSale"
-            title="Web Page for Sale"
-            thumbnail={webPageForSale}
+          <WorkGridItem id="webDeveloperBlog" title="Web Developer Blog" thumbnail={webDeveloperBlog}
           >
+            A personal blog about web development.
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+
+      <Divider my={6} />
+
+      <Heading as="h3" variant="section-title">
+        Projects
+      </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section delay={0.1}>
+          <WorkGridItem id="forSale" title="Web Page for Sale" thumbnail={webPageForSale}>
             This website is for sale and is aimed at the passenger
             transportation niche.
           </WorkGridItem>
         </Section>
+        <Section>
+          <WorkGridItem id="wCsJS" title="Web Components" thumbnail={wCsJS}>
+                A website built with Web Component technology and in the Latin American language.
+            </WorkGridItem>
+        </Section>
+        <Section>
+            <WorkGridItem id="f101bs" title="Finanzas 101 Bots" thumbnail={f101bs}>
+                The website of an algorithmic trader. This site implements a chatbot specialized in financial matters.
+            </WorkGridItem>
+        </Section>
+        <Section delay={0.1}>
+            <WorkGridItem id="blogs" title="Blogs" thumbnail={blogs}>
+                A website that integrates 3 blogs. This site is loaded dynamically from basic HTML, CSS and JavaScript technologies by manipulating the DOM.
+            </WorkGridItem>
+        </Section>
       </SimpleGrid>
-      {/*             <Heading as="h3" fontSize={20} mb={4}>
-                Projects
-            </Heading>
-            <SimpleGrid columns={[1, 1, 2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="wCsJS" title="Web Components" thumbnail={wCsJS}>
-                        A website built with Web Component technology and in the Latin American language.
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="f101bs" title="Finanzas 101 Bots" thumbnail={f101bs}>
-                        The website of an algorithmic trader. This site implements a chatbot specialized in financial matters.
-                    </WorkGridItem>
-                </Section>
-                <Section delay={0.1}>
-                    <WorkGridItem id="blogs" title="Blogs" thumbnail={blogs}>
-                        A website that integrates 3 blogs. This site is loaded dynamically from basic HTML, CSS and JavaScript technologies by manipulating the DOM.
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid> */}
 
-      <Heading mt="7" as="h3" fontSize={20} mb={4}>
-        {/*         <Box borderRadius="lg"
-          borderWidth={2}
-          borderStyle="solid"
-          mb={6}
-          p={3}
-          textAlign="center"
-          bg={useColorModeValue('whiteAlpha.500', 'blackAlpha.500')}>
- */}{' '}
+    <Divider my={6} />
+
+      <Heading as="h3" variant="section-title">
         Networking
-        {/*         </Box> */}
       </Heading>
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section delay={0.1}>
