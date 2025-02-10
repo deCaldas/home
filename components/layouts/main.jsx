8c53c671266bca9b4792b8ae56eb408e4a9f29f3
Diego Whiskey <d.calcar@hotmail.com>
2025-02-10 16:17:25 -0500

    [UPDATE] open graph image in main.jsx

--- components/layouts/main.jsx
+++ components/layouts/main.jsx
@@ -33,14 +33,14 @@ const Main = ({ children, router }) => {
         <meta name="twitter:creator" content="@d_caldasCaridad" />
         <meta
           name="twitter:image"
-          content="https://github.com/deCaldas/home/blob/main/public/og/opengraph-imalogo.png"
+          content="https://github.com/deCaldas/home/blob/main/pages/opengraph-imalogo.png?raw=true"
         />
         <meta property="og:site_name" content="Diego Whiskey" />
         <meta name="og:title" content="Diego Whiskey" />
         <meta property="og:type" content="website" />
         <meta
           property="og:image"
-          content="https://github.com/deCaldas/home/blob/main/public/og/opengraph-imalogo.png"
+          content="https://github.com/deCaldas/home/blob/main/pages/opengraph-imalogo.png?raw=true"
         />
         <title>Diego&apos;s Homepage</title>
       </Head>
