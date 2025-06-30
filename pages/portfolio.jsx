afcd8318ee3ef3d164e1eb2cc076181dee9bd8b8
Diego Whiskey <d.calcar@hotmail.com>
2025-06-30 18:54:45 -0500

    [MODIFY] Divider of Chakra UI and borderColor with useColorModeValue style

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -6,94 +6,97 @@ import {
   Box,
   Heading,
   SimpleGrid,
-  Divider,
   useColorModeValue,
-  AspectRatio
+  AspectRatio,
+  Divider
 } from '@chakra-ui/react'
 import LayoutMain from '../components/layouts/main'
-import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import Section from '../components/section'
+import { Meta } from '../components/jobsTemplate'
+import MatrixRain from '../components/MatrixRain'
+
 /* Imgs */
 import contactNet_Angeliana from '../public/images/contactNet_Angeliana.png'
 import mostlyEntertainment from '../public/images/mostlyEntertainment.png'
 import landingsForAutomotiveWorkshops from '../public/images/landingsForAutomotiveWorkshops.png'
-import MatrixRain from '../components/MatrixRain'
-import { Meta } from '../components/jobsTemplate'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const Development = () => (
   <LayoutMain title="portfolio">
     <Container mt="73" mb="23">
-      <Heading mt="7" as="h3" fontSize={20} mb={4}>
-        <Box
-          borderRadius="lg"
-          borderWidth={2}
-          borderStyle="solid"
-          mb={6}
-          p={3}
-          textAlign="center"
-          bg={useColorModeValue('whiteAlpha.500', 'blackAlpha.500')}
-        >
-          {' Hola Mundo!! '}
-        </Box>
-      </Heading>
+      <Section>
+        <Heading mt="7" as="h3" fontSize={20} mb={4}>
+          <Box
+            borderRadius="lg"
+            borderWidth={2}
+            borderStyle="solid"
+            mb={6}
+            p={3}
+            textAlign="center"
+            bg={useColorModeValue('whiteAlpha.500', 'blackAlpha.500')}
+            >
+            {' Hello World! '}
+          </Box>
+        </Heading>      
 
-      <Divider my={6} />
+        <AspectRatio ratio={7}>
+          <MatrixRain />
+        </AspectRatio>
+      </Section>
 
-      <AspectRatio ratio={1.7} my={4}>
-        <MatrixRain />
-      </AspectRatio>
+      <Divider borderColor={useColorModeValue('blackAlpha.700', 'whiteAlpha.700')} marginY={7} />
 
-      <Divider my={6} />
+      <Section>
+        <Heading as="h3" variant="section-title">
+          Projects
+        </Heading>
 
-      <Heading as="h3" variant="section-title">
-        Projects
-      </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.1}>
+            <WorkGridItem
+              id="landingsFor-automotive-workshops"
+              title="Landings for Automotive Workshop"
+              thumbnail={landingsForAutomotiveWorkshops}
+              >
+              This&apos;s self-employment, landing page funnel aimed at automotive
+              workshops
+            </WorkGridItem>
+          </Section>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.1}>
-          <WorkGridItem
-            id="landingsFor-automotive-workshops"
-            title="Landings for Automotive Workshop"
-            thumbnail={landingsForAutomotiveWorkshops}
-          >
-            This&apos;s self-employment, landing page funnel aimed at automotive
-            workshops
-          </WorkGridItem>
-        </Section>
+          <Section delay={0.1}>
+            <WorkGridItem
+              id="mostlyEntertainment"
+              title="Mostly Entertainment Home Studio"
+              thumbnail={mostlyEntertainment}
+              >
+              Artistic project. The website design is based on the principles of
+              simplicity, elegance and ease of use. And multimedia content
+              integration to enhance user experience
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Section>
 
-        <Section delay={0.1}>
-          <WorkGridItem
-            id="mostlyEntertainment"
-            title="Mostly Entertainment Home Studio"
-            thumbnail={mostlyEntertainment}
-          >
-            Artistic project. The website design is based on the principles of
-            simplicity, elegance and ease of use. And multimedia content
-            integration to enhance user experience
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
+      <Section>
+        <Heading as="h3" variant="section-title">
+          Networking
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.1}>
+            <WorkGridItem
+              id="contactNet_Angeliana"
+              title="Angeliana Alvarez Homepage"
+              thumbnail={contactNet_Angeliana}
+              >
+              Web page to my friend. This page is made possible by Next.js and
+              Chakra UI.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Section>
 
-      <Divider my={6} />
-
-      <Heading as="h3" variant="section-title">
-        Networking
-      </Heading>
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.1}>
-          <WorkGridItem
-            id="contactNet_Angeliana"
-            title="Angeliana Alvarez Homepage"
-            thumbnail={contactNet_Angeliana}
-          >
-            Web page to my friend. This page is made possible by Next.js and
-            Chakra UI.
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-
-      <Divider my={6} />
+      <Divider borderColor={useColorModeValue('blackAlpha.700', 'whiteAlpha.700')} marginY={7} />
 
       <Box
         display={{ md: 'flex' }}
@@ -127,6 +130,7 @@ const Development = () => (
           </ListItem>
         </List>
       </Box>
+      <Divider borderColor={useColorModeValue('blackAlpha.700', 'whiteAlpha.700')} marginY={7} />
     </Container>
   </LayoutMain>
 )
